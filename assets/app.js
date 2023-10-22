ce7ae0e5d27570f6002a79591c0d7c1861f7508a
Eva López <evamlopez1981@gmail.com>
2023-10-22 21:04:56 +0200

    added exercise: html_forms

---
+++ assets/app.js
@@ -0,0 +1,25 @@
+/* function result() {
+    
+    var name = document.getElementById('name').value;
+    var email = document.getElementById('email').value;
+    var age = document.getElementById('age').value;
+    var description = document.getElementById('description').value;
+    var recommend = document.getElementById('recommend').value;
+    var comment = document.getElementById('comment').value;
+    
+    console.log(name+ ", "+email+", "+age+", "+description+", "+recommend+", "+comment);
+    } */
+
+
+   function result() {
+
+        document.write(name+"<br/>");
+        document.write(email+"<br/>");
+        document.write(age+"<br/>");
+        document.write(description+"<br/>");
+        document.write(recommend+"<br/>");
+        document.write(comment+"<br/>");
+   }
+
+   result();
+
