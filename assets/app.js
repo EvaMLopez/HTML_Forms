eaefe954870e519886752ba2850f91356792fd1d
Eva López <evamlopez1981@gmail.com>
2023-11-07 20:54:09 +0100

    testing

--- assets/app.js
+++ assets/app.js
@@ -1,25 +1,12 @@
-/* function result() {
-    
-    var name = document.getElementById('name').value;
-    var email = document.getElementById('email').value;
-    var age = document.getElementById('age').value;
-    var description = document.getElementById('description').value;
-    var recommend = document.getElementById('recommend').value;
-    var comment = document.getElementById('comment').value;
-    
-    console.log(name+ ", "+email+", "+age+", "+description+", "+recommend+", "+comment);
-    } */
-
-
-   function result() {
-
-        document.write(name+"<br/>");
-        document.write(email+"<br/>");
-        document.write(age+"<br/>");
-        document.write(description+"<br/>");
-        document.write(recommend+"<br/>");
-        document.write(comment+"<br/>");
-   }
-
-   result();
+function result() {    
+   document.querySelector("form")
+    .addEventListener("submit", e => {
+      e.preventDefault()
+      const data = Object.fromEntries(
+        new FormData(e.target)
+      )
+      console.log(data)
+    }
+    )
+  }
 
